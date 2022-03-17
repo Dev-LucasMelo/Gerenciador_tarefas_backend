abdde13f4367987739de5c34fdebd795e5353ce8
Dev-LucasMelo <lmelo072002@gmail.com>
2022-03-17 16:17:16 -0300

    rota da plataforma

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended:false }))
 app.use(express.static(__dirname + '/public')) //css na pasta public
 app.use('/',Router)
-
+app.use(express.json())
+app.use(express.Router())
 //port 
 app.listen(4000 || process.env.PORT, ()=>{
     console.log('rodando na porta 4000')
--- routes/Routes.js
+++ routes/Routes.js
@@ -3,19 +3,29 @@ const Router = express.Router()
 const database = require('./../database/database')
 
 
+//ROTAS DE DADOS
 
-Router.get('/datalogin', (req, res) => {
-    var login = database.query('select * from login')
-
-    login.then((data) => {
+Router.get('/datacadastro', (req, res) => {
+    var cadastro = database.query('select * from login')
+    cadastro.then((data) => {
         res.json(data.rows)
     })
-   
 })
 
-Router.get('/dataplat', (req, res) => {
-    //Rota geral da api
-    res.json('dados plataforma')
+Router.get('/tasks4',(req,res)=>{
+    var platdata = database.query('select * from tasks4')
+    platdata.then((data)=>{
+        res.json(data.rows)
+    })
+})
+    
+//ROTAS DE REQUISIÇÕES 
+
+Router.post('/cadastro', (req, res) => {
+    //AQUI ADICIONA AO BANCO DE DADOS   
+    database.query('insert into login (username,password,email,sexo) values ($1,$2,$3,$4)',[req.body.username,req.body.password,req.body.email,req.body.sex])
+
+    res.redirect('http://localhost:3000/finish')
 })
 
 
