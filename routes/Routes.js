d66e44a2d5b20a352abe0518cf124e601e8e0881
Dev-LucasMelo <lmelo072002@gmail.com>
2022-03-21 14:53:38 -0300

    criação de rotas dinamicas para cada usuario

--- routes/Routes.js
+++ routes/Routes.js
@@ -12,21 +12,45 @@ Router.get('/datacadastro', (req, res) => {
     })
 })
 
-Router.get('/tasks4',(req,res)=>{
-    var platdata = database.query('select * from tasks4')
-    platdata.then((data)=>{
-        res.json(data.rows)
+var teste = database.query('select * from login')
+
+teste.then((data) => {
+
+    var rotasdeuser = data.rows
+
+    rotasdeuser.forEach((item) => {
+
+        const username = item.username.replace(/\s/g, '').toLowerCase()
+
+        Router.get(`/tasks${username}`, (req, res) => {
+            var tasksuser = database.query(`select * from tasks${username}`)
+            tasksuser.then((data) => {
+                res.json(data.rows)
+            })
+        })
+
     })
 })
-    
+
+
+
+
 //ROTAS DE REQUISIÇÕES 
 
 Router.post('/cadastro', (req, res) => {
     //AQUI ADICIONA AO BANCO DE DADOS   
-    database.query('insert into login (username,password,email,sexo) values ($1,$2,$3,$4)',[req.body.username,req.body.password,req.body.email,req.body.sex])
+    database.query('insert into login (username,password,email,sexo) values ($1,$2,$3,$4)', [req.body.username, req.body.password, req.body.email, req.body.sex])
+
+    database.query(`create table tasks${req.body.username} ( 
+        id serial,
+        tittletask varchar,
+        contenttask varchar,
+        completed boolean
+    ) `)
 
     res.redirect('http://localhost:3000/finish')
 })
+
 
 
 module.exports = Router
